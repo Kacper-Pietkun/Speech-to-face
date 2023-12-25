b414b188e8a6a201c0379b2a0682b7abe1592262
Kacper-Pietkun <kacper.pietkun00@gmail.com>
2023-12-25 14:47:18 +0100

    Adjust ModelSaver for fintuning

--- src/train/model_saver.py
+++ src/train/model_saver.py
@@ -6,7 +6,8 @@ class ModelSaver():
     def __init__(self, save_latest_path, save_best_path, best_loss=math.inf):
         self.best_loss = best_loss
         self.save_latest_path = save_latest_path 
-        self.save_best_path = save_best_path 
+        self.save_best_path = save_best_path
+        self.fine_tune_epoch_start = -1
         
     def save(self, loss, epoch, model_state_dict, optimizer_state_dict, history, latest):
         state = {
@@ -15,7 +16,8 @@ class ModelSaver():
             "best_loss": self.best_loss,
             "model_state_dict": model_state_dict,
             "optimizer_state_dict": optimizer_state_dict,
-            "history": history
+            "history": history,
+            "fine_tune_epoch_start": self.fine_tune_epoch_start
         }
 
         if loss < self.best_loss:
@@ -24,3 +26,6 @@ class ModelSaver():
             torch.save(state, self.save_best_path)
         if latest is True:
             torch.save(state, self.save_latest_path)
+
+    def start_finetune(self, epoch):
+        self.fine_tune_epoch_start = epoch
